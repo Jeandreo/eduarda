686134074ceacc6f2fcea41dbe16e78cec8ada09
JeandreoFurquim <jeandreofur@gmail.com>
2024-08-09 23:46:22 -0300

    First

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,83 +24,83 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('@Sucesso1243'),
         ]);
 
-        User::factory()->create([
-            'name' => 'Jeandreo da Cruz Furquim',
-            'email' => 'jeandreofur@gmail.com',
-            'password' => Hash::make('Inc@ns4v3l_2024'),
-        ]);
+        // // User::factory()->create([
+        // //     'name' => 'Jeandreo da Cruz Furquim',
+        // //     'email' => 'jeandreofur@gmail.com',
+        // //     'password' => Hash::make('Inc@ns4v3l_2024'),
+        // // ]);
 
-        Gift::create([
-            'name' => 'Jogo de Louça',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Jogo de Louça',
+        // // ]);
 
-        Gift::create([
-            'name' => 'Toalhas',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Toalhas',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Conjunto de Talheres',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Conjunto de Talheres',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Jogo de Panelas',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Jogo de Panelas',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Aparelho de Jantar',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Aparelho de Jantar',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Cafeteira Elétrica',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Cafeteira Elétrica',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Liquidificador',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Liquidificador',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Panela de Pressão',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Panela de Pressão',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Faqueiro',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Faqueiro',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Assadeiras',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Assadeiras',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Conjunto de Taças',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Conjunto de Taças',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Copos de Vidro',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Copos de Vidro',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Jogo de Lençol',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Jogo de Lençol',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Frigideira Antiaderente',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Frigideira Antiaderente',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Edredom',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Edredom',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Travessa de Vidro',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Travessa de Vidro',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Cortinas',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Cortinas',
+        // // ]);
         
-        Gift::create([
-            'name' => 'Tapete',
-        ]);
+        // // Gift::create([
+        // //     'name' => 'Tapete',
+        // // ]);
 
     }
 }
