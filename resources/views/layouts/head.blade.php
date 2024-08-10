796d457e2b9d9cf017c92f22f15a89f5181230f0
JeandreoFurquim <jeandreofur@gmail.com>
2024-08-09 23:59:09 -0300

    First

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,14 +1,14 @@
 <base href="">
 <title>@yield('title-page', 'Eduarda') - Chá de Casa Nova</title>
-<meta name="description" content="DreamFlow é a solução definitiva para o gerenciamento eficiente de suas tarefas. Organize, acompanhe e conclua seus projetos com facilidade." />
-<meta name="keywords" content="gerenciamento de tarefas, produtividade, organização, software, colaboração, equipe, projetos" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta charset="utf-8" />
 <meta property="og:locale" content="pt_BR" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="DreamFlow - Gerenciamento de Tarefas Simplificado" />
-<meta property="og:url" content="https://dreamake.com.br/" />
-<meta property="og:site_name" content="Keenthemes | Metronic" />
+<meta name="description" content="description of your website/webpage, make sure you use keywords!" />
+<meta property="og:title" content="short title of your website/webpage" />
+<meta property="og:url" content="https://www.example.com/webpage/" />
+<meta property="og:description" content="description of your website/webpage" />
+<meta property="og:image" content="//cdn.example.com/uploads/images/webpage_300x200.png" />
+
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
 <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.webp') }}" />
